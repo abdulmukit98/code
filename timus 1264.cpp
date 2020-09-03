7c25a0aab57e51e1ef0cd130d1f7343c91d9d629
Abdul Mukit <56398175+abdulmukit98@users.noreply.github.com>
2020-09-03 17:04:50 +0600

    Create timus 1264.cpp

---
+++ timus 1264.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n , m ;
+    scanf("%d %d", &n, &m);
+
+    // from 0 to m  total
+    m = m + 1;
+
+    //n = element array
+
+    //total second need  n x m
+
+    printf("%d", n*m);
+
+    return 0;
+}
