29b7217fe19305c1e927af4c521ca2e82ada5297
abdulmukit98 <mdabdulmukit98@gmail.com>
2020-10-22 11:51:41 +0600

    java

---
+++ timus/timus 1000 A+B problem.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class haleon {
+    /*
+        timus 1000 A+B Problem
+     */
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a,b;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.println(a+b);
+    }
+}
